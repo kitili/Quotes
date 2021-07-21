1b88c46b8b4e05237dd3710b2e174857ad0a4d2c
Your Name <you@example.com>
2021-07-21 17:15:49 +0300

    initial commit

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -11,7 +11,7 @@ export class QuotesComponent implements OnInit {
   quotes: Quotes[] = [
     new Quotes(1, 'Audrey',"success","success depends on the number of times you get back to raise a fallen fellow you are with in the journey", 'Angel',new Date(2020,3,14),0,0),
     new Quotes(2,'Trolls',"despair","sometimes crying is what it takes to be strong ",'Trichet',new Date(2020,3,14),0,0),
-    new Quotes(3,'Get new Phone Case',"kitili","kitili",'Diana has her birthday coming up soon',new Date(2020,3,14),0,0)
+    new Quotes(3,'Rasim',"life","life is a bitch",'a..s',new Date(2020,3,14),0,0)
    
   ];
   
