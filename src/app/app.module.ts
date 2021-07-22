e29bfc00f789a4eefa6666a3478b29dd35404514
Your Name <you@example.com>
2021-07-22 07:53:23 +0300

    final commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
 import { DetailsComponent } from './details/details.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
-import { DirectiveComponent } from './directive/directive.component';
+
 
 @NgModule({
   declarations: [
@@ -15,7 +15,7 @@ import { DirectiveComponent } from './directive/directive.component';
     QuotesComponent,
     DetailsComponent,
     QuoteFormComponent,
-    DirectiveComponent
+  
   ],
   imports: [
     BrowserModule,
